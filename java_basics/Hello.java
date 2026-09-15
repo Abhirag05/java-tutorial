db4fb2a9bb3a19145a3a13b39e714edadada3a34
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-09-15 13:14:41 +0530

    Add Hello.java with a simple greeting program

---
+++ java_basics/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        String name = "Abhi";
+        int age = 20;
+        System.out.println("Hello" + " " + name + " " + "Your age is:" + age);
+    }
+}
