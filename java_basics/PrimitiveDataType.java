18b03da6f05bd2a357f1569baaf28c6e53767562
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-09-15 13:54:10 +0530

    Add PrimitiveDataType.java to demonstrate Java's primitive data types

---
+++ java_basics/PrimitiveDataType.java
@@ -0,0 +1,21 @@
+//java has 2 data types:primitive and reference
+//There are 8 primitive data types 
+/*
+1byte-8bits
+
+Integer:byte(8bit),short(16bit),int(32bit),long(64bit)
+Float:float(4byte),double
+Character:char(2byte)
+Boolean:boolean
+*/
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+        float marks = 6.5f;// by default point values are double need to put f for making the floatand also
+                           // long needs l
+        System.out.println(marks);
+        char c = 'h';
+        System.out.println(c);
+        boolean val = true;
+        System.out.println(val);
+    }
+}
