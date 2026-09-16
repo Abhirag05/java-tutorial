e23b7b229411e0ed9574d91913041c6748d3e0c3
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-09-16 12:34:44 +0530

    switch

---
+++ java_basics/Switch.java
@@ -0,0 +1,16 @@
+public class Switch {
+    public static void main(String[] args) {
+        int val=1;
+        switch (val) {
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("monday");
+                break;
+            default:
+                System.out.println("not defined");
+                break;
+        }
+    }   
+}
