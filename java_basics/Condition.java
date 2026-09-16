14cf40dbf6943077d2f203ee1d58c869441dff01
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-09-16 12:21:53 +0530

    if conditon

---
+++ java_basics/Condition.java
@@ -0,0 +1,11 @@
+public class Condition {
+    public static void main(String[] args) {
+        if(5>6){
+            System.out.println("5>6");
+        }
+        else{
+            System.out.println("not");
+       }
+        
+    }
+}
