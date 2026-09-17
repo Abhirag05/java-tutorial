a17b97db826cca0fabdbf7d8b8040ba0cbfd2066
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-09-17 22:40:55 +0530

    enhanced_loop

---
+++ java_basics/EnhancedForLoop.java
@@ -0,0 +1,8 @@
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        int nums[]={1,2,3,4,5};
+        for (int arr : nums){
+            System.out.println(arr);
+        }
+    }
+}
