1b69dbd3a7aecf125b388439040878052eead10c
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-09-17 11:19:02 +0530

    for

---
+++ java_basics/ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i=1; i<=5;i++){
+            System.out.println("Day:"+i);
+            for(int t=9;t<=18;t++){
+                System.out.println("Time:"+t);
+            }//inner for loop
+        }
+    }
+}
+
+
+
