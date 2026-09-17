81cf898b3d9bb528e0364f0c04ec90da0be56cd6
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-09-17 10:55:38 +0530

    do while

---
+++ java_basics/DoWhile.java
@@ -0,0 +1,12 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int n=0;
+        int i=1;
+        do{
+            System.out.println("printing:"+i);
+            i++;
+        }
+        while(i<=n);
+    }
+}
+//the difference between the while and do while is in the while only the statements get executed if the condition is true but in the do while it will get executed atleast once even if the condition is not true
